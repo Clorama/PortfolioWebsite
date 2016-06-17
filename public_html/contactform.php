0a9f357895779b336ce32da08e535cb176adc287
clorama dorvilias <cloramadorvilias@pc52.home>
2016-06-17 16:03:50 +0200

    adding all files

---
+++ public_html/contactform.php
@@ -0,0 +1,104 @@
+<!doctype html> 
+<html lang="en">
+
+	<head> 
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49334808-1', 'creativeclo.com');
+  ga('send', 'pageview');
+
+</script>
+		<title>Creative Clo - Portfolio Website - Welcome </title>
+	</head>
+
+	<body>
+			<header> <div class="blackheader">
+		  <div id="logo">
+                 <a class="social fb" href="https://www.facebook.com/Clorama" title="Facebook Me!" alt="Facebook Icon" target="_blank">Facebook</a>
+            <a class="social2 twit" href="https://www.twitter.com/creativeclo" title="Twitter" alt="Twitter Icon" target="_blank">Twitter</a>
+            <a class="social3 li" href="http://www.linkedin.com/pub/clorama-dorvilias/21/388/a62" title="LinkedIn" alt="LinkedIn Icon" target="_blank">LinkedIn</a>
+					<hgroup>
+						<center><h1 class="dot_text">Creative Clo!</h1>
+							<h2>&#9776;</h2>
+					</hgroup>
+				<div>
+
+				<div id="nav_bar">
+					  <nav>
+                        <center><a href="index.html"class="circle">Welcome</a>  
+                        <a href="sales.html" class="circle">My Work</a> 
+                        <a href="aboutme.html" class="circle">About Me</a> 
+                        <a href="projects.html" class="circle">Projects</a>
+                        <a href="http://www.interactiondesignwithclo.tumblr.com" target="_blank" class="circle">Blog</a>
+                        <a href="contactform.php" class="circle selected">Contact</a> 
+                         </center>
+                    </nav>
+				</div>
+			</div><!--black header-->
+			</header>
+
+
+
+		<center>
+			
+			<div class="midplus textcon clearfix">	
+<div class="contactform">
+			<form method="post" action="mailer.php" class="contact">
+				
+				<input type="text" name="name" Placeholder="Name or Organization">
+			
+				<input type="email" name="email" Placeholder="E-mail Address" required>
+
+				<textarea rows="10" name="message" Placeholder="How can I help you?"></textarea>
+
+				<input type="text" name="verify" placeholder="Verify you're human" class="verifybox" required>
+
+				<img class="verifyimg" src="verificationimage.php? <php? echo rand(0,9999);?>" alt="verification image">
+			
+				<input type="submit" value="Send">
+
+			</form>
+		</div> <!--end contactform-->
+	
+	
+			<div class="contact_text">
+						<p><em><strong>Get in touch with me!</strong></em><p> 
+							<br>
+							<br>
+						<p>I strongly believe attractive design and cool visuals can engage people to support a good cause. <b>Do you have a good cause that needs promotion? Need a creativity boost or consulting for a marketing strategy to get your initiative off the ground? </b> Please let me know how I can be of service! 
+						<br>
+						(And high five to you--seriously, <a href="http://www.daimanuel.com/wp-content/uploads/2011/03/virtual-high-five.jpg" target="_blank">High Five<a>!)</p>
+						<br>
+						
+			</div><!--contact text-->	
+
+
+				</div>
+
+				<div class="social_links">
+							<a href="http://www.Facebook.com/clorama" title="Facebook" target="_blank" class="fbcontact">Facebook</a>
+							<a href="http://www.twitter.com/CreativeClo" title="Twitter" target="_blank" class="twcontact">Twitter</a>
+							<a href="http://www.instagram.com/clobama" title="Instagram" target ="_blank" class="igcontact">Instagram</a>
+							<a href="http://www.creative-clo.tumblr.com" title="Tumblr" target="_blank" class="tcontact">Tumblr</a>
+							<a href="http://www.linkedin.com/pub/clorama-dorvilias/21/388/a62/" title="Linkedin" target="_blank" class="linkcontact">Linked In</a>
+		
+
+					</div><!-- social links -->
+
+			
+
+
+
+  <footer><center>
+        &copy; 2014 Clorama Dorvilias. All Rights Reserved. | Designed &amp; Coded by <a href="http://www.astrangecloud.com" target="_self">Clorama Dorvilias</a>
+    </center></footer>
+
+	</body>
+</html>
